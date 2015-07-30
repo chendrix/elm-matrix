89bca81e26fde501b1f2d83821013d0ab0834248
Chris Hendrix <cah276@cornell.edu>
2015-07-30 01:22:33 -0700

    Add verbose output

--- TestRunner.elm
+++ TestRunner.elm
@@ -1,6 +1,6 @@
 import IO.IO exposing (..)
 import IO.Runner exposing (Request, Response, run)
-import Check.Runner.Console exposing (runDisplay)
+import Check.Runner.Console exposing (runDisplay, runDisplayVerbose)
 import Check exposing (..)
 
 import MatrixTest
@@ -15,6 +15,6 @@ result = quickCheck tests
 
 
 port requests : Signal Request
-port requests = run responses (runDisplay result)
+port requests = run responses (runDisplayVerbose result)
 
 port responses : Signal Response
--- test/Check/Runner/Console.elm
+++ test/Check/Runner/Console.elm
@@ -1,5 +1,6 @@
 module Check.Runner.Console
   ( runDisplay
+  , runDisplayVerbose
   ) where
 {-| Console test runner for elm-check. This module provides functions to
 run tests in the console.
@@ -15,17 +16,24 @@ import String
 
 import IO.IO exposing (..)
 
-(:::) = (,)
+
+{-| Display verbose test results in the console.
+-}
+runDisplayVerbose : Evidence -> IO ()
+runDisplayVerbose = runDisplay' True
 
 
 {-| Display test results in the console.
 -}
 runDisplay : Evidence -> IO ()
-runDisplay evidence =
+runDisplay = runDisplay' False
+
+runDisplay' : Bool -> Evidence -> IO ()
+runDisplay' verbose evidence =
   let
     summary = ""
     allPassed = isOk evidence
-    results = display' 0 evidence
+    results = display' verbose 0 evidence
     out = summary ++ "\n\n" ++ (String.concat << List.intersperse "\n" <| results)
   in
     putStrLn out >>>
@@ -34,16 +42,16 @@ runDisplay evidence =
         False -> exit 1
 
 
-display' : Int -> Evidence -> List String
-display' n evidence = case evidence of
+display' : Bool -> Int -> Evidence -> List String
+display' verbose n evidence = case evidence of
   Unit unitEvidence ->
-    displayUnit n unitEvidence
+    displayUnit verbose n unitEvidence
   Multiple name evidences ->
-    displaySuite n name evidences
+    displaySuite verbose n name evidences
 
 
-displaySuite : Int -> String -> List Evidence -> List String
-displaySuite n name evidences =
+displaySuite : Bool -> Int -> String -> List Evidence -> List String
+displaySuite verbose n name evidences =
   let
     allPassed = areOk evidences
     msg = "Test Suite: " ++ name ++ ": "
@@ -51,7 +59,7 @@ displaySuite n name evidences =
     subResults =
       if allPassed
       then []
-      else List.concatMap (display' (n + 2)) evidences
+      else List.concatMap (display' verbose (n + 2)) evidences
   in
     indent n msg :: subResults
 
@@ -70,8 +78,8 @@ areOk evidences =
   List.all ((==) True) (List.map isOk evidences)
 
 
-displayUnit : Int -> UnitEvidence -> List String
-displayUnit n unitEvidence =
+displayUnit : Bool -> Int -> UnitEvidence -> List String
+displayUnit verbose n unitEvidence =
   case unitEvidence of
     Ok options ->
       [indent n (successMessage options)]
@@ -86,16 +94,18 @@ displayUnit n unitEvidence =
           ]
 
         verboseParts =
-          [ "Seed: " ++ (toString options.seed.state)
-          , "Number of shrinking operations performed: " ++ (toString options.numberOfShrinks)
-          , "Before shrinking: "
-          ] ++
-          (List.map (indent 2)
-            [ "Counter example: " ++ options.original.counterExample
-            , "Actual: " ++ options.original.actual
-            , "Expected: " ++ options.original.expected
-            ]
-          )
+          if not verbose then []
+          else
+            [ "Seed: " ++ (toString options.seed.state)
+            , "Number of shrinking operations performed: " ++ (toString options.numberOfShrinks)
+            , "Before shrinking: "
+            ] ++
+            (List.map (indent 2)
+              [ "Counter example: " ++ options.original.counterExample
+              , "Actual: " ++ options.original.actual
+              , "Expected: " ++ options.original.expected
+              ]
+            )
       in
         failMessage options
         :: List.map (indent 2) (essentialParts ++ verboseParts)
@@ -106,6 +116,7 @@ failMessage : FailureOptions -> String
 failMessage options =
   options.name ++ " FAILED after " ++ (toString options.numberOfChecks) ++ " check"++ (if options.numberOfChecks == 1 then "" else "s") ++ "!"
 
+
 successMessage : SuccessOptions -> String
 successMessage {name, seed, numberOfChecks} =
   name ++ " passed after " ++ (toString numberOfChecks) ++ " checks."
@@ -115,11 +126,13 @@ successMessage {name, seed, numberOfChecks} =
 vcat : List String -> String
 vcat = String.concat << List.intersperse "\n"
 
+
 replicate : Int -> Char -> String
 replicate n c = let go n = if n <= 0
                            then []
                            else c :: go (n - 1)
                 in String.fromList << go <| n
+
 
 indent : Int -> String -> String
 indent n = let indents = replicate n ' '
